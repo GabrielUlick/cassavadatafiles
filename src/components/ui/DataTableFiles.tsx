3e0091a71fb4066aa74d5b15197af03540c5bd77
Gabriel Ulick <gabrieludb@hotmail.com>
2023-11-22 21:17:28 -0300

    fix: search datagrid

--- src/components/ui/DataTableFiles.tsx
+++ src/components/ui/DataTableFiles.tsx
@@ -186,10 +186,10 @@ export function DataTableFiles() {
     <div className="w-full">
       <div className="flex items-center py-4">
         <Input
-          placeholder="Filter emails..."
-          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+          placeholder="Search"
+          value={(table.getColumn("filename")?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
-            table.getColumn("email")?.setFilterValue(event.target.value)
+            table.getColumn("filename")?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
