a46c94d4e9dbf22bb0d4ff194c2304fae49722ba
Gabriel Ulick <gabrieludb@hotmail.com>
2023-11-22 22:36:32 -0300

    agora vai

--- src/app/(Login)/page.tsx
+++ src/app/(Login)/page.tsx
@@ -13,38 +13,38 @@ export default function Home() {
   const [email, setEmail] = useState("");
 
   const handleSignIn = async () => {
-  try {
-    if (email === "") {
-      throw new Error("Falha no Login: Digite seu Email.");
+    try {
+      if (email === "") {
+        throw new Error("Falha no Login: Digite seu Email.");
+      }
+
+      const endpoint = "sessions";
+      const requestData = { email };
+
+      const response = await api.post(endpoint, requestData, {
+        validateStatus: (status) => status < 405,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer", // Adicione seu token de autorização aqui
+        },
+      });
+
+      if (response.status !== 200) {
+        throw new Error(response.data.message);
+      }
+
+      const uid = response.data.uid;
+      console.log("UUID recebido:", uid);
+
+      // Condição para verificar se o localStorage está disponível (no navegador)
+      if (typeof window !== "undefined" && window.localStorage) {
+        localStorage.setItem("uid", uid);
+      }
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+      alert(error);
     }
-
-    const endpoint = "sessions";
-    const requestData = { email };
-
-    const response = await api.post(endpoint, requestData, {
-      validateStatus: (status) => status < 405,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer", // Adicione seu token de autorização aqui
-      },
-    });
-
-    if (response.status !== 200) {
-      throw new Error(response.data.message);
-    }
-
-    const uid = response.data.uid;
-    console.log("UUID recebido:", uid);
-
-    // Condição para verificar se o localStorage está disponível (no navegador)
-    if (typeof window !== "undefined" && window.localStorage) {
-      localStorage.setItem("uid", uid);
-    }
-  } catch (error) {
-    console.error("Erro ao fazer login:", error);
-    alert(error);
-  }
-};
+  };
 
   return (
     <div className="flex items-center justify-center bg-white h-[78vh] w-auto">
